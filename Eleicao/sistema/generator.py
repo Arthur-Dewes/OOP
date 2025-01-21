e514edc165f75889cfa798fe47ecd2c18825956b
Arthur Dewes <arthurgdewes@gmail.com>
2025-01-21 14:05:09 -0300

    ajuste gen

--- Eleicao/sistema/generator.py
+++ Eleicao/sistema/generator.py
@@ -11,7 +11,7 @@ import random
 #arq urnas: cpf, depFe, depEs, sena, gove, pres
 
 
-def gen_files(output_dir: str):
+def gen_files(output_dir: str, num_pessoas: int) -> None:
     def gen_invalid_vote(min_value: int, max_value: int, lista: list):
         while True:
             num_aleatorio = random.randint(min_value, max_value)
@@ -28,10 +28,10 @@ def gen_files(output_dir: str):
 
     f.add_provider(propostas_provider)
 
-    nomes = np.array([f.name() for _ in range(1000)])
+    nomes = np.array([f.name() for _ in range(num_pessoas)])
     pessoas = {'Nome': nomes,
-            'Idade': [f.random_int(min=18, max=70) for _ in range(1000)],
-            'Cpf': [f.cpf() for _ in range(1000)]}
+            'Idade': [f.random_int(min=18, max=70) for _ in range(num_pessoas)],
+            'Cpf': [f.cpf() for _ in range(num_pessoas)]}
 
     depFe = [f.unique.random_int(min=1000, max=9999) for _ in range(70)]
     depEs = [f.unique.random_int(min=10000, max=99999) for _ in range(94)]
@@ -66,16 +66,18 @@ def gen_files(output_dir: str):
     df3 = pd.DataFrame(partidos)
 
     # acrescentando erros ao arquivo
-    random_pos_eleitores = np.array([f.unique.random_int(min=0, max=999) for _ in range(13)])
-    random_pos_urnas = [f.unique.random_int(min=0, max=499) for _ in range(5)]
+    random_pos_eleitores = np.array([f.unique.random_int(min=2, max=num_pessoas+1) for _ in range(13)])
+    random_pos_urnas = [f.unique.random_int(min=0, max=num_pessoas) for _ in range(5)]
     invalid_cpfs = ["123.456.789-00", "111.111.111-11", "000.000.000-00", "222.222.222-22", "333.444.555-66",
                     "999.888.777-00", "555.123.456-78", "444.444.444-00", "123.123.123-11", "678.901.234-56"]
 
-    # - 3 ajusta o índice para o iloc
-    df1.iloc[random_pos_eleitores[:10] - 3, 2] = invalid_cpfs # cpfs inválidos
-    df1.iloc[random_pos_eleitores[10] - 3, 1] = 17 # menor de idade
-    df1.iloc[random_pos_eleitores[11] - 3, 0] = "" # nome vazio
-    df1.iloc[random_pos_eleitores[12] - 3, 2] = "" # cpf vazio
+    # - 2 ajusta o índice para o iloc
+    random_pos_eleitores = random_pos_eleitores - 2
+    
+    df1.iloc[random_pos_eleitores[:10], 2] = invalid_cpfs # cpfs inválidos
+    df1.iloc[random_pos_eleitores[10], 1] = 17 # menor de idade
+    df1.iloc[random_pos_eleitores[11], 0] = "" # nome vazio
+    df1.iloc[random_pos_eleitores[12], 2] = "" # cpf vazio
 
     # voto inválido para cada cargo
     novo_depFe = gen_invalid_vote(1000, 9999, depFe)
@@ -86,7 +88,7 @@ def gen_files(output_dir: str):
 
     with open(f'{output_dir}/eleitores.txt', 'x') as file:
         file.write('Pessoas: \n')
-        for i in range(1000):
+        for i in range(num_pessoas):
             file.write(f"{df1.iloc[i]['Nome']},{df1.iloc[i]['Idade']},{df1.iloc[i]['Cpf']}\n")
         file.write('Candidatos: \n')
         for i in range(209):
@@ -95,27 +97,30 @@ def gen_files(output_dir: str):
         for i in range(32):
             file.write(f"{df3.iloc[i]['Nome']},{df3.iloc[i]['Num_eleitoral']},{df3.iloc[i]['Cnpj']}\n")
 
+    num_urnas = num_pessoas // 500
     reorg = df1['Cpf'].tolist()
     random.shuffle(reorg)
 
-    with open(f'{output_dir}/urna1.txt', 'x') as file:
-        file.write('Urna 1: \n')
-        for i in range(500):
-            if i == random_pos_urnas[0] - 2:
-                file.write(f"{reorg[i]},{novo_depFe},{random.choice(df2.iloc[70:164]['Num_voto'].values)},{random.choice(df2.iloc[164:184]['Num_voto'].values)},{random.choice(df2.iloc[184:194]['Num_voto'].values)},{random.choice(df2.iloc[194:]['Num_voto'].values)}\n")
-            elif i == random_pos_urnas[1] - 2:
-                file.write(f"{reorg[i]},{random.choice(df2.iloc[:70]['Num_voto'].values)},{novo_depEs},{random.choice(df2.iloc[164:184]['Num_voto'].values)},{random.choice(df2.iloc[184:194]['Num_voto'].values)},{random.choice(df2.iloc[194:]['Num_voto'].values)}\n")
-            elif i == random_pos_urnas[2] - 2:
-                file.write(f"{reorg[i]},{random.choice(df2.iloc[:70]['Num_voto'].values)},{random.choice(df2.iloc[70:164]['Num_voto'].values)},{novo_sena},{random.choice(df2.iloc[184:194]['Num_voto'].values)},{random.choice(df2.iloc[194:]['Num_voto'].values)}\n")
-            else:
-                file.write(f"{reorg[i]},{random.choice(df2.iloc[:70]['Num_voto'].values)},{random.choice(df2.iloc[70:164]['Num_voto'].values)},{random.choice(df2.iloc[164:184]['Num_voto'].values)},{random.choice(df2.iloc[184:194]['Num_voto'].values)},{random.choice(df2.iloc[194:]['Num_voto'].values)}\n")
+    count = 0
+    # Gerando arquivos de urnas
+    for i in range(num_urnas if num_pessoas % 500 == 0 else num_urnas + 1):
+        start_idx = i * 500
+        end_idx = min((i + 1) * 500, num_pessoas)
 
-    with open(f'{output_dir}/urna2.txt', 'x') as file:
-        file.write('Urna 2: \n')
-        for i in range(500):
-            if i == random_pos_urnas[3] - 2:
-                file.write(f"{reorg[i + 500]},{random.choice(df2.iloc[:70]['Num_voto'].values)},{random.choice(df2.iloc[70:164]['Num_voto'].values)},{random.choice(df2.iloc[164:184]['Num_voto'].values)},{novo_gove},{random.choice(df2.iloc[194:]['Num_voto'].values)}\n")
-            elif i == random_pos_urnas[4] - 2:
-                file.write(f"{reorg[i + 500]},{random.choice(df2.iloc[:70]['Num_voto'].values)},{random.choice(df2.iloc[70:164]['Num_voto'].values)},{random.choice(df2.iloc[164:184]['Num_voto'].values)},{random.choice(df2.iloc[184:194]['Num_voto'].values)},{novo_pres}\n")
-            else:
-                file.write(f"{reorg[i + 500]},{random.choice(df2.iloc[:70]['Num_voto'].values)},{random.choice(df2.iloc[70:164]['Num_voto'].values)},{random.choice(df2.iloc[164:184]['Num_voto'].values)},{random.choice(df2.iloc[184:194]['Num_voto'].values)},{random.choice(df2.iloc[194:]['Num_voto'].values)}\n")
+        with open(f'{output_dir}/urna{i + 1}.txt', 'x') as file:
+            file.write(f'Urna {i + 1}: \n')
+            for _ in range(start_idx, end_idx):
+                if count == random_pos_urnas[0] - 2:
+                    file.write(f"{reorg[count]},{novo_depFe},{random.choice(df2.iloc[70:164]['Num_voto'].values)},{random.choice(df2.iloc[164:184]['Num_voto'].values)},{random.choice(df2.iloc[184:194]['Num_voto'].values)},{random.choice(df2.iloc[194:]['Num_voto'].values)}\n")
+                elif count == random_pos_urnas[1] - 2:
+                    file.write(f"{reorg[count]},{random.choice(df2.iloc[:70]['Num_voto'].values)},{novo_depEs},{random.choice(df2.iloc[164:184]['Num_voto'].values)},{random.choice(df2.iloc[184:194]['Num_voto'].values)},{random.choice(df2.iloc[194:]['Num_voto'].values)}\n")
+                elif count == random_pos_urnas[2] - 2:
+                    file.write(f"{reorg[count]},{random.choice(df2.iloc[:70]['Num_voto'].values)},{random.choice(df2.iloc[70:164]['Num_voto'].values)},{novo_sena},{random.choice(df2.iloc[184:194]['Num_voto'].values)},{random.choice(df2.iloc[194:]['Num_voto'].values)}\n")
+                elif count == random_pos_urnas[3] - 2:
+                    file.write(f"{reorg[count]},{random.choice(df2.iloc[:70]['Num_voto'].values)},{random.choice(df2.iloc[70:164]['Num_voto'].values)},{random.choice(df2.iloc[164:184]['Num_voto'].values)},{novo_gove},{random.choice(df2.iloc[194:]['Num_voto'].values)}\n")
+                elif count == random_pos_urnas[4] - 2:
+                    file.write(f"{reorg[count]},{random.choice(df2.iloc[:70]['Num_voto'].values)},{random.choice(df2.iloc[70:164]['Num_voto'].values)},{random.choice(df2.iloc[164:184]['Num_voto'].values)},{random.choice(df2.iloc[184:194]['Num_voto'].values)},{novo_pres}\n")
+                else:
+                    file.write(f"{reorg[count]},{random.choice(df2.iloc[:70]['Num_voto'].values)},{random.choice(df2.iloc[70:164]['Num_voto'].values)},{random.choice(df2.iloc[164:184]['Num_voto'].values)},{random.choice(df2.iloc[184:194]['Num_voto'].values)},{random.choice(df2.iloc[194:]['Num_voto'].values)}\n")
+                count += 1
+    return

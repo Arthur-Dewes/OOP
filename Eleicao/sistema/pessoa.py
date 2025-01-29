b7e2fb730a5b43e0a463b2c2b4e67b685cc7a087
Arthur Dewes <arthurgdewes@gmail.com>
2025-01-28 23:09:31 -0300

    decorador e sem validação interna

--- Eleicao/sistema/pessoa.py
+++ Eleicao/sistema/pessoa.py
@@ -1,29 +1,39 @@
-from excecoes import NotPythonNameError, AgeError, CpfError
+from .excecoes import NotPythonNameError, AgeError, CpfError
 
 class Pessoa:
-    """Classe que armeza informações de uma pessoa"""
-    
+    """Classe que armazena informações de uma pessoa"""
+
     def __init__(self, name: str, idade: int, cpf: str):
-        if not name.isalpha() and len(name) >= 1:
-            raise NotPythonNameError(name)
-        if not (18 < idade < 120):
-            raise AgeError(idade)
-        if not CpfError(cpf).cpf[1]:
-            raise CpfError(cpf)
-        
-        # se tiver raise, o objeto não é criado
-        self.name = name
-        self.idade = idade
-        self.cpf = cpf
-        
+        self._name = name
+        self._idade = idade
+        self._cpf = cpf
+
     @property
     def name(self):
         return self._name
+
+    @name.setter
+    def name(self, value: str):
+        if type(value) != str or len(value) <= 1:
+            raise NotPythonNameError(value)
+        self._name = value
 
     @property
     def idade(self):
         return self._idade
 
+    @idade.setter
+    def idade(self, value: int):
+        if not (18 < value < 120):
+            raise AgeError(value)
+        self._idade = value
+
     @property
     def cpf(self):
         return self._cpf
+
+    @cpf.setter
+    def cpf(self, value: str):
+        if not CpfError(value).cpf[1]:
+            raise CpfError(value)
+        self._cpf = value

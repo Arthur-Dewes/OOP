a854884abb588966459ca536b1691f1e515a88f6
Arthur Dewes <arthurgdewes@gmail.com>
2025-01-30 22:54:35 -0300

    correção candidatos

--- Eleicao/sistema/candidatos.py
+++ Eleicao/sistema/candidatos.py
@@ -9,41 +9,41 @@ class Candidatos(ABC, Pessoa):
         pass
 
     def __init__(self, nome: str, idade: int, cpf: str, numero_voto: int, proposta: str):
-        Pessoa._init_(self, nome, idade, cpf)
+        super().__init__(nome, idade, cpf)
         self.numero_voto = numero_voto
         self.proposta = proposta
 
 
 class DepFederal(Candidatos):
-    def verificar_numero_candidato(self):
+    def verificar_numero_candidato(self) -> bool:
         if isinstance(self.numero_voto, int) and len(str(self.numero_voto)) == 4:
             return True # voto válido
         else:
             return False # voto inválido
 
 class DepEstadual(Candidatos):
-    def verificar_numero_candidato(self):
+    def verificar_numero_candidato(self) -> bool:
         if isinstance(self.numero_voto, int) and len(str(self.numero_voto)) == 5:
             return True # voto válido
         else:
             return False # voto inválido
 
 class Senador(Candidatos):
-    def verificar_numero_candidato(self):
+    def verificar_numero_candidato(self) -> bool:
         if isinstance(self.numero_voto, int) and len(str(self.numero_voto)) == 3:
             return True # voto válido
         else:
             return False # voto inválido
 
 class Governador(Candidatos):
-    def verificar_numero_candidato(self):
+    def verificar_numero_candidato(self) -> bool:
         if isinstance(self.numero_voto, int) and len(str(self.numero_voto)) == 2:
             return True # voto válido
         else:
             return False # voto inválido
 
 class Presidente(Candidatos):
-    def verificar_numero_candidato(self):
+    def verificar_numero_candidato(self) -> bool:
         if isinstance(self.numero_voto, int) and len(str(self.numero_voto)) == 2:
             return True # voto válido
         else:
